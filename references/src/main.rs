16933fdd894dcb3b86927343336378427f70c7bc
Graham Strickland <grahamhstrickland@gmail.com>
2025-12-15 21:02:48 +0200

    Add remaining references examples

--- references/src/main.rs
+++ references/src/main.rs
@@ -215,8 +215,45 @@ fn main() {
 
     assert_eq!(wave, vec![0.0, 1.0, 0.0, -1.0]);
 
-    extend(&mut wave, &wave);
-    assert_eq!(wave, vec![0.0, 1.0, 0.0, -1.0, 0.0, 1.0, 0.0, -1.0]);
+    // extend(&mut wave, &wave);
+    // assert_eq!(wave, vec![0.0, 1.0, 0.0, -1.0, 0.0, 1.0, 0.0, -1.0]);
+
+    let mut x = 10;
+    let r1 = &x;
+    let r2 = &x; // ok: multiple shared borrows permitted
+    // x += 10;                    // error: cannot assign to `x` because it is borrowed
+    // let m = &mut x;   // error: cannot borrow `x` as mutable because it is
+    //                             // also borrowed as immutable
+    // println!("{}, {}, {}", r1, r2, m);  // the references are used here,
+    //                                     // so their lifetimes must last
+    //                                     // at least this long
+
+    let mut y = 20;
+    let m1 = &mut y;
+    // let m2 = &mut y;  // error: cannot borrow as mutable more than once
+    // let z = y;             // error: cannot use `y` because it was mutably borrowed
+    // println!("{}, {}, {}", m1, m2, z);  // references are used here
+
+    let mut w = (107, 109);
+    let r = &w;
+    let r0 = &r.0;      // ok: reborrowing shared as shared
+    // let m1 = &mut r.1;    // error: can't reborrow shared as mutable
+    println!("{}", r0);             // r0 gets used here
+
+    let mut v = (136, 139);
+    let m = &mut v;
+    let m0 = &mut m.0;    // ok: reborrowing mutable from mutable
+    *m0 = 137;
+    let r1 = &m.1;            // ok: reborrowing shared from mutable
+                                    // and doesn't overlap with m0
+    // v.1;                            // error: access through other paths still forbidden
+    println!("{}", r1);             // r1 gets used here
+
+    let mut x = 42;    // non-const i32 variable
+    let p = &x;       // shared reference to i32
+    assert_eq!(*p, 42);
+    // x += 1;                 // error: cannot assign to x because it is borrowed
+    assert_eq!(*p, 42);     // if you take out the assignment, this is true
 }
 
 fn show(table: &Table) {
